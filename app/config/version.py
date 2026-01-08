5d76b8383219a3f52d310c9760e277205f65247e
digipost069-make <digipost069@gmail.com>
2026-01-08 15:18:40 +0700

    update url github

--- app/config/version.py
+++ app/config/version.py
@@ -1,2 +1,2 @@
 VERSION = "26.0.1"
-VERSION_URL = "https://raw.githubusercontent.com/YOUR_GITHUB_USERNAME/YOUR_REPO_NAME/main/version.json"
+VERSION_URL = "https://raw.githubusercontent.com/digipos069-maker/video-downloader-desktop/main/version.json"
